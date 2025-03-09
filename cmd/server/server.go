5f61950df81ac3029990512aa69f1600b8cf4db5
s19835 <s19835@sci.pdn.ac.lk>
2025-03-09 18:57:49 +0530

    add: test handler

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,18 +3,25 @@ package server
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-// handler function for root ("/")
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func testHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the home page")
 }
 
 func Start() {
-	http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/", testHandler)
 
-	fmt.Println("Server is running on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Failed to start server:", err)
-	}
+	// fmt.Println("Server is running on http://localhost:8080")
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	fmt.Println("Failed to start server:", err)
+	// }
+
+	router := mux.NewRouter()
+	router.HandleFunc("/test", testHandler)
+
+	http.Handle("/", router)
+	fmt.Println("Everything is set up!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/s19835/http_serve_go
 
 go 1.24.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
